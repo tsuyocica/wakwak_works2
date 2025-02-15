c07f15ec8b83d421cbe4330eb72762a396a7a3b1
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-02-15 20:55:54 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show] do # マイページ用
     member do
-      # get :show_owner  # 依頼者用ページ
-      # get :show_worker # 作業員用ページ
+      get :show_manager  # 施工管理者用ページ
+      get :show_worker   # 作業員用ページ
     end
   end
 
-  resources :job_posts, only: [:index, :show, :new, :create] do # 作業員募集機能
+  resources :job_posts do # 作業員募集機能
     resources :job_applications, only: [:create] # 応募処理は job_post にネスト
   end
 end
